def4d19c1ed95a4c78e59bc351ca9197e215059f
ericsonnascimento <ericson@grupocyber.com.br>
2025-04-12 08:40:53 -0300

    style: estilização do componente form

--- src/componets/Form/index.js
+++ src/componets/Form/index.js
@@ -4,8 +4,9 @@ import './Form.css'
 
 const Form = () => {
     return (
-        <section>
+        <section className='form'>
             <form>
+                <h2>Preencha os dados para criar o card do pet</h2>
                 <FieldText label="Nome" placeholder="Digite o nome do PET" />
                 <FieldText label="Raça" placeholder="Digite a raça do PET" />
                 <FieldText label="Imagem" placeholder="Digite o endereço da imagem do seu PET" />
